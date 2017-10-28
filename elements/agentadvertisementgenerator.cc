6295beaa2bae9530deeaba8083050e374418d559
Michael Adriaensen <michaeladriaensen@gmail.com>
2017-10-28 16:51:10 +0200

    made first version of agentadvertisementgenerator.

---
+++ elements/agentadvertisementgenerator.cc
@@ -0,0 +1,103 @@
+#include <click/config.h>
+#include <click/args.hh>
+#include <click/error.hh>
+#include "agentadvertisementgenerator.hh"
+#include "mobile_ip_packets.h"
+#include <clicknet/ip.h>
+#include <clicknet/ether.h>
+#include <click/timer.hh>
+#include <clicknet/icmp.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+
+CLICK_DECLS
+
+AgentAdvertisementGenerator::AgentAdvertisementGenerator() : MABase(0), _sequence(0) {}
+AgentAdvertisementGenerator::~AgentAdvertisementGenerator() {}
+
+int AgentAdvertisementGenerator::configure(Vector<String>& conf, ErrorHandler* errh) {
+    if (Args(conf, this, errh)
+    .read("MABASE", MABase)
+    .complete() < 0) return -1;
+
+    if (MABase == 0) return errh->error("Wrong  argument, should be a MAInfoBase element.");
+    return 0;
+}
+
+int AgentAdvertisementGenerator::sendAgentAdvertisement(const String& conf, Element* e, void* thunk, ErrorHandler* errh){
+    AgentAdvertisementGenerator* aag =  (AgentAdvertisementGenerator*) e;
+    if (Packet *q = aag->make_packet()) {
+ 	    aag->output(0).push(q);
+        click_chatter("send agent advertisement with broadcast destination\n");
+    }
+    return 0;
+}
+  
+void AgentAdvertisementGenerator::add_handlers() {
+    add_write_handler("sendAdvertisement", &sendAgentAdvertisement, (void*)0);
+}
+
+Packet* AgentAdvertisementGenerator::make_packet() {
+	int headroom = sizeof(click_ether);
+    
+    WritablePacket *q = Packet::make(headroom, 0, sizeof(click_ip) 
+    + sizeof(icmp_router_advertisement) + sizeof(agent_advertisement_extension)
+    + sizeof(router_address_preference_level) + sizeof(IPAddress) , 0);
+
+    if (!q)
+        return 0;
+    memset(q->data(), '\0', sizeof(click_ip) + sizeof(icmp_router_advertisement)
+    + sizeof(agent_advertisement_extension));
+
+    click_ip *iph = (click_ip *)q->data();
+    
+    iph->ip_v = 4;
+    iph->ip_hl = sizeof(click_ip) >> 2;
+    iph->ip_len = htons(q->length());
+    uint16_t ip_id = ((_sequence) % 0xFFFF) /*+ 1*/; // ensure ip_id != 0
+    iph->ip_id = htons(ip_id);
+    iph->ip_p = IP_PROTO_ICMP; /* icmp */
+    iph->ip_ttl = 1;
+    iph->ip_src = MABase->getMyPrivateAddress();
+    iph->ip_dst = IPAddress("255.255.255.255");
+    iph->ip_sum = click_in_cksum((unsigned char *)iph, sizeof(click_ip));
+    
+    icmp_router_advertisement *icmph = (icmp_router_advertisement *)(iph + 1);
+    
+    icmph->type = ICMP_ROUTERADVERT;
+    icmph->code = 0;
+    icmph->checksum = 0;
+    icmph->num_addrs = 1;
+    icmph->addr_entry_size = 2;
+    icmph->lifetime = htons(45);
+
+    router_address_preference_level *rapl = (router_address_preference_level*)(icmph + 1);
+
+    rapl->router_address = MABase->getMyPublicAddress();
+    rapl->preference_level = htonl(1);
+
+    agent_advertisement_extension *aa_ext = (agent_advertisement_extension*)(rapl + 1);
+
+    aa_ext->type = 16;
+    aa_ext->length = 6 + 4 * 1;
+    aa_ext->sequence_number = htons(_sequence);
+    aa_ext->registration_lifetime = htons(30);
+    aa_ext->flags_and_reserved = htons(0b1011000000000000);
+
+    IPAddress *coa = (IPAddress*)(aa_ext + 1);
+    *coa = MABase->getMyPublicAddress();
+
+    icmph->checksum = click_in_cksum((const unsigned char *)icmph, sizeof(icmp_router_advertisement)
+    + sizeof(router_address_preference_level) + sizeof(agent_advertisement_extension) + sizeof(IPAddress));
+
+    _sequence++; 
+    
+    q->set_dst_ip_anno(IPAddress("255.255.255.255"));
+    
+    return q;
+   
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(AgentAdvertisementGenerator)
---
+++ elements/agentadvertisementgenerator.hh
@@ -0,0 +1,44 @@
+#ifndef CLICK_AGENTADVERTISEMENTGENERATOR_HH
+#define CLICK_AGENTADVERTISEMENTGENERATOR_HH
+
+#include <click/config.h>
+#include <click/args.hh>
+#include <click/error.hh>
+#include <clicknet/ip.h>
+#include <clicknet/ether.h>
+#include <click/timer.hh>
+#include <click/ipaddress.hh>
+#include <click/vector.hh>
+#include <click/hashtable.hh>
+#include "mainfobase.hh"
+#include "mobile_ip_packets.h"
+
+CLICK_DECLS
+
+class AgentAdvertisementGenerator : public Element {
+public:
+    AgentAdvertisementGenerator();
+    ~AgentAdvertisementGenerator();
+
+    const char* class_name() const { return "AgentAdvertisementGenerator"; }
+    const char* port_count() const { return "0/1"; }
+    const char* processing() const { return PUSH; }
+    int configure(Vector<String>&, ErrorHandler*);
+
+    void sendGeneralReport(int maxRespTime);
+
+    static int sendAgentAdvertisement(const String& conf, Element* e, void* thunk, ErrorHandler* errh);
+    void add_handlers();
+
+
+private:
+
+    Packet* make_packet();
+    MAInfoBase* MABase;
+
+    uint32_t _sequence;
+    
+};
+
+CLICK_ENDDECLS
+#endif
